150071e278fe5b0d339ac4a3a0a6a55ff04c5288
nenoNaninu <neno@melonpan.pro>
2025-01-25 22:23:16 +0900

    add filter option

--- src/AspNetCore.SignalR.OpenTelemetry/HubInstrumentationOptions.cs
+++ src/AspNetCore.SignalR.OpenTelemetry/HubInstrumentationOptions.cs
@@ -1,9 +1,28 @@
 using System;
 using System.Diagnostics;
+using Microsoft.AspNetCore.SignalR;
 
 namespace AspNetCore.SignalR.OpenTelemetry;
 
 public sealed class HubInstrumentationOptions
 {
     public Action<Activity, Exception>? OnException { get; set; }
+
+    /// <summary>
+    /// Gets or sets a filter function that determines whether or not to collect telemetry on a per invocation basis.
+    /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>
+    /// If filter is <see langword="null" />, all invocations are collected.
+    /// </item>
+    /// <item>
+    /// If filter returns <see langword="true" />, the invocation is collected.
+    /// </item>
+    /// <item>
+    /// If filter returns <see langword="false" /> the invocation is NOT collected.
+    /// </item>
+    /// </list>
+    /// </remarks>
+    public Func<HubInvocationContext, bool>? Filter { get; set; }
 }
